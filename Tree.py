7ccf341229b7e10d184b34b3746850f9359ece90
Leonardo Wellausen <lwellausen@gmail.com>
2018-01-13 08:55:12 -0200

    fully working

--- Tree.py
+++ Tree.py
@@ -1,13 +1,14 @@
-import math, random
+import math, random, statistics
 
-train_size = 0.6
-validation_size = 0.2
+train_size = 0.8
 test_size = 0.2
+k = 10
 
 ensemble_size = 3
 m = 3
 
 dataset = []
+train = []
 test = []
 validation = []
 classes_num = 0
@@ -187,14 +188,28 @@ def info(dj):
 
 def infoa(d, a_index):
     sum = 0.0
-    for j in attributes_values[a_index]:
-        dj = []
-        for element in d:
-            if element[0][a_index] == j:
-                dj.append(element)
-        dj_norm = dj.__len__()/d.__len__()
+    if attributes_type[a_index] == nominal:
+        for j in attributes_values[a_index]:
+            dj = []
+            for element in d:
+                if element[0][a_index] == j:
+                    dj.append(element)
+            dj_norm = dj.__len__()/d.__len__()
 
-        sum += dj_norm * info(dj)
+            sum += dj_norm * info(dj)
+
+    elif attributes_type[a_index] == numerical:
+        d1 = []
+        d2 = []
+        for element in d:
+            if float(element[0][a_index]) <= attributes_values[a_index][0]:
+                d1.append(element)
+            else:
+                d2.append(element)
+        d1_norm = d1.__len__() / d.__len__()
+        d2_norm = d2.__len__() / d.__len__()
+
+        sum = (d1_norm * info(d1)) + (d2_norm * info(d2))
 
     return sum
 
@@ -240,15 +255,38 @@ def induction(d, l):
     n.setAtt(a)  # 4.2
     l.remove(a)  # 4.3
 
-    for v in attributes_values[a]:
-        dv = [instance for instance in d if instance[0][a] == v]  #  4.4.1
+    if attributes_type[a] == nominal:
+        for v in attributes_values[a]:
+            dv = [instance for instance in d if instance[0][a] == v]  #  4.4.1
 
-        if dv.__len__() == 0:
+            if dv.__len__() == 0:
+                leaf = Node()
+                leaf.setClass(max(classes_freq, key=lambda key: classes_freq[key]))
+                n.children.append(leaf)
+            else:
+                n.children.append(induction(dv, l[:]))
+    elif attributes_type[a] == numerical:
+        d1 = []
+        d2 = []
+        for element in d:
+            if float(element[0][a]) <= attributes_values[a][0]:
+                d1.append(element)
+            else:
+                d2.append(element)
+
+        if d1.__len__() == 0:
             leaf = Node()
             leaf.setClass(max(classes_freq, key=lambda key: classes_freq[key]))
             n.children.append(leaf)
         else:
-            n.children.append(induction(dv, l[:]))
+            n.children.append(induction(d1, l[:]))
+
+        if d2.__len__() == 0:
+            leaf = Node()
+            leaf.setClass(max(classes_freq, key=lambda key: classes_freq[key]))
+            n.children.append(leaf)
+        else:
+            n.children.append(induction(d2, l[:]))
 
     return n
 
@@ -262,7 +300,7 @@ def classifier(node: Node, instance: list):   # instance has only attributes, no
                     return classifier(node.children[i], instance)
 
         elif attributes_type[att] == numerical:
-            if instance[att] <= node.values:
+            if float(instance[att]) <= node.values[0]:
                 return classifier(node.children[0], instance)
             else:
                 return classifier(node.children[1], instance)
@@ -273,12 +311,12 @@ def classifier(node: Node, instance: list):   # instance has only attributes, no
     return
 
 
-def genBootstraps(m):
+def genBootstraps(ntree, l):
     bootstraps = []
-    for i in range(m):
+    for i in range(ntree):
         bootstrap = ([], [])  #  uma tupla com treino e teste
-        bootstrap[0].extend(random.choices(dataset, k=dataset.__len__()))
-        bootstrap[1].extend([inst for inst in dataset if inst not in bootstrap[0]])
+        bootstrap[0].extend(random.choices(l, k=l.__len__()))
+        bootstrap[1].extend([inst for inst in l if inst not in bootstrap[0]])
         bootstraps.append(bootstrap)
 
     return bootstraps
@@ -292,7 +330,7 @@ def majorityVoting(forest, instance):
     return max(votes, key=lambda key: votes[key])
 
 
-def testForest(forest, test_data):
+def testForest(forest, test_data):   # testa a floresta no conjunto dado e retorna a acurácia
     acc = 0.0
     for inst in test_data:
         if inst[1] == majorityVoting(forest, inst[0]):
@@ -301,15 +339,118 @@ def testForest(forest, test_data):
     return acc/test_data.__len__()
 
 
-if __name__ == '__main__':
-    read_benchmark()
-    #  read_haberman()
-    #  read_cmc()
-    #  read_wine()
+def splitDataset():
+    size = dataset.__len__()
 
-    bootstraps = genBootstraps(ensemble_size)
+    for i in range(int(train_size*size)):
+        index = random.randint(0, dataset.__len__()-1)
+        train.append(dataset.pop(index))
+
+    test.extend(dataset)
+
+    dataset.clear()
+
+    return
+
+
+def resolve(att):
+    data = train[:]
+    data = sorted(data, key=lambda x: x[0][att])
+
+    results = {}  # dict com valor de corte: entropia
+
+    for i in range(data.__len__()-1):
+        if data[i][1] != data[i+1][1]:  # instancia consecutivas com calss divergente
+            mean = (float(data[i][0][att]) + float(data[i+1][0][att]))/2
+
+            if mean not in list(results.keys()):
+                attributes_values[att] = [mean]  # temporariamente seta esse valor para facilitar o uso da func infoa
+                results[mean] = infoa(data, att)  # avalia o corte encontrado em t0do o conj de treino
+
+    attributes_values[att] = [min(results, key=lambda key: results[key])]
+
+    return
+
+
+def resolve_numericals():
+    for att in attributes:
+        if attributes_type[att] == numerical:
+            resolve(att)
+
+    return
+
+def genFolds(l):    # divide o conjunto de treino em k folds
+    #  fold_size = int(math.ceil(train.__len__()/k))
+    l_size = l.__len__()
+    folds = []
+
+    for i in range(k):
+        fold = []
+        for j in range(l_size//k):
+            if l.__len__() != 0:
+                fold.append(l.pop(0))
+        folds.append(fold)
+
+    if l.__len__() != 0 :   # sobrou algum item porque a lista não é divisivel igualmente por k
+        folds[k-1].extend(l)  # botamos o resto de l no último fold
+
+    return folds
+    #  return [train[i:i + fold_size] for i in range(0, train.__len__(), fold_size)]
+
+
+def single_cross(ntree, folds):   # realiza o cross validaton para um conjunto de parâmetros. retorna médio e dp
+    accuracies = []
+    for i in range(k):
+        cross_test = folds[i]
+
+        cross_train = []
+        for f in range(k):  # gera conjunto de treino com os folds que não são o de teste
+            if f != i:
+                cross_train.extend(folds[f])
+
+        bootstraps = genBootstraps(ntree, cross_train)
+        forest = [induction(bootstrap[0], attributes[:]) for bootstrap in bootstraps]
+
+        accuracies.append(testForest(forest, cross_test))
+
+    return statistics.mean(accuracies), statistics.pstdev(accuracies)
+
+
+def cross_validation():
+    folds = genFolds(train[:])
+    ntree_vals = [10, 25, 50, 60]
+    results = {}  # dict ntree: (media, desvio)
+
+    for ntree in ntree_vals:
+        results[ntree] = single_cross(ntree, folds)
+
+    ntree = max(results, key=lambda key: results[key][0])
+
+    bootstraps = genBootstraps(ntree, train)    # treina uma floresta usando o valor ótimo e com conj treino inteiro
     forest = [induction(bootstrap[0], attributes[:]) for bootstrap in bootstraps]
 
-    print(majorityVoting(forest, dataset[0][0]))
-    print(testForest(forest, dataset))
+    return forest
+
+
+if __name__ == '__main__':
+    #read_benchmark()
+    read_haberman()
+    #  read_cmc()
+    #  read_wine()
+    random.seed("cidy")
+    m = int(math.sqrt(attributes.__len__()))   #  dica do relatório
+
+    splitDataset()
+
+    resolve_numericals()
+
+    forest = cross_validation()
+
+    print(testForest(forest, test))
+
+    #  bootstraps = genBootstraps(ensemble_size)
+    #  forest = [induction(bootstrap[0], attributes[:]) for bootstrap in bootstraps]
+
+    #  print(majorityVoting(forest, train[0][0]))
+    #  print(testForest(forest, train))
     print("haha")
